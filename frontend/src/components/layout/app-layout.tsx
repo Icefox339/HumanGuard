70cc9aad7da0cab16942f7e19a227110a7a6f7f6
Icefox339 <87526304+Icefox339@users.noreply.github.com>
2026-05-22 13:19:13 +0300

    Adapt frontend layout for mobile and toggle build badge

--- frontend/src/components/layout/app-layout.tsx
+++ frontend/src/components/layout/app-layout.tsx
@@ -1,19 +1,24 @@
+import { useState } from 'react';
 import { Header } from '@/components/layout/header';
 import { Sidebar } from '@/components/layout/sidebar';
 import { BuildVersion } from '@/components/layout/build-version';
 import { RouteTransition } from '@/components/layout/route-transition';
 import { NavigationProgress } from '@/components/layout/navigation-progress';
 
-export const AppLayout = () => (
-  <div className="app-shell flex min-h-screen text-[rgb(var(--text-primary))]">
-    <Sidebar />
-    <div className="flex flex-1 flex-col">
-      <Header />
-      <NavigationProgress />
-      <main className="p-6 pb-14">
-        <RouteTransition />
-      </main>
+export const AppLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  return (
+    <div className="app-shell flex min-h-screen text-[rgb(var(--text-primary))]">
+      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+      <div className="flex flex-1 flex-col">
+        <Header onToggleSidebar={() => setIsSidebarOpen((prev) => !prev)} />
+        <NavigationProgress />
+        <main className="p-4 pb-20 sm:p-6">
+          <RouteTransition />
+        </main>
+      </div>
+      <BuildVersion />
     </div>
-    <BuildVersion />
-  </div>
-);
+  );
+};
